9f8a9a0eb02fda148b3dba71efb1c02b02a60259
Krzysztof Brymer <kbrymer@groupon.com>
2020-06-10 17:48:30 +0200

    Handling posting items that already exists | status codes

--- code/app.py
+++ code/app.py
@@ -10,16 +10,14 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        # for item in items:
-        #     if item["name"] == name:
-        #         return item
-        item = list(filter(lambda x: x["name"] == name, items))
-        if item:
-            return item
-        else:
-            return {"item": None}, 404
+        item = next(filter(lambda x: x["name"] == name, items), None)
+
+        return {"item": item}, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x["name"] == name, items), None) is not None:
+            return {"message": "Item with name '{}' already exists.".format(name)}, 400
+
         data = request.get_json()
         item = {"name": name, "price": data["price"]}
         items.append(item)
