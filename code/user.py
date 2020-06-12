adf7b1959df8405f090906fc857d3af64407e25d
sonny-skyez <krzysztof.brymer@gmail.com>
2020-06-12 19:43:23 +0200

    Added registration functionality and new API resource

--- code/user.py
+++ code/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 
 class User:
@@ -34,3 +35,21 @@ class User:
         return user
 
 
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("username", type=str, required=True, help="This field cannot be blank.")
+    parser.add_argument("password", type=str, required=True, help="This field cannot be blank.")
+
+    def post(self):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        data = UserRegister.parser.parse_args()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data["username"], data["password"]))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "User '{}' created successfully".format(data["username"])}, 201
