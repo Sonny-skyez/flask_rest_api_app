3756c40e8f2f24704dafd1fcbfa3fa0bee277cd4
sonny-skyez <krzysztof.brymer@gmail.com>
2020-06-12 20:58:12 +0200

    Extracted 'insert' method from POST method

--- code/item.py
+++ code/item.py
@@ -37,6 +37,15 @@ class Item(Resource):
         data = Item.parser.parse_args()
         item = {"name": name, "price": data["price"]}
 
+        try:
+            self.insert(item)
+        except:
+            return {"message": "An error occurred while trying to insert item"}, 500    # internal server error
+
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
@@ -46,12 +55,17 @@ class Item(Resource):
         connection.commit()
         connection.close()
 
-        return item, 201
-
     # @jwt_required()
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x["name"] != name, items))
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
         return {"message": "Item with name '{}' deleted.".format(name)}
 
     # @jwt_required()
