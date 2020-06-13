cecc607033c8b5894c6259f640dfaf9575677a45
sonny-skyez <krzysztof.brymer@gmail.com>
2020-06-13 23:20:01 +0200

    Store model and resource added

--- code/resources/item.py
+++ code/resources/item.py
@@ -8,6 +8,9 @@ class Item(Resource):
     parser.add_argument(
         "price", type=float, required=True, help="This field cannot be left blank.",
     )
+    parser.add_argument(
+        "store_id", type=int, required=True, help="Every item need a store id..",
+    )
 
     @jwt_required()
     def get(self, name):
@@ -22,7 +25,7 @@ class Item(Resource):
             return {"message": "Item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data["price"])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -49,7 +52,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data["price"])
+            item = ItemModel(name, **data)
         else:
             item.price = data["price"]
 
