84c777e57e1fa8637df0f5cde6737f378472e03b
sonny-skyez <krzysztof.brymer@gmail.com>
2020-06-13 02:19:20 +0200

    Added models and resources | code refactoring

--- code/models/user.py
+++ code/models/user.py
@@ -1,8 +1,7 @@
 import sqlite3
-from flask_restful import Resource, reqparse
 
 
-class User:
+class UserModel:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
@@ -33,25 +32,3 @@ class User:
 
         connection.close()
         return user
-
-
-class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument("username", type=str, required=True, help="This field cannot be blank.")
-    parser.add_argument("password", type=str, required=True, help="This field cannot be blank.")
-
-    def post(self):
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-
-        data = UserRegister.parser.parse_args()
-        if User.find_user_by_username(data["username"]):
-            return {"message": "Username already taken. Please try again."}, 400
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data["username"], data["password"]))
-
-        connection.commit()
-        connection.close()
-
-        return {"message": "User '{}' created successfully".format(data["username"])}, 201
