3b60769a91d256d630406fda440959f7adb4585e
sonny-skyez <krzysztof.brymer@gmail.com>
2020-06-13 22:12:13 +0200

    Database tables created automatically before first request

--- code/app.py
+++ code/app.py
@@ -14,6 +14,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "Kris"
 api = Api(app)
 
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Item, "/item/<string:name>")
