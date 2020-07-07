2b4df684be165f3a983019716f50af6a4b21e8ed
sonny-skyez <krzysztof.brymer@gmail.com>
2020-07-07 15:04:57 +0200

    Added environment variable in app.py file

--- code/app.py
+++ code/app.py
@@ -14,7 +14,7 @@ from models.item import ItemModel
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = "Kris"
+app.secret_key = os.environ.get("SECRET_KEY")
 api = Api(app)
 
 
